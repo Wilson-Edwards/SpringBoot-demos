5d743c56a3fb1dcc7110107141f1b8631cea5d24
lin <784264272@qq.com>
2019-11-19 10:29:30 +0800

    users.sql文件修正

--- springboot_lombok/users.sql
+++ springboot_lombok/users.sql
@@ -1,20 +1,34 @@
+/*
+Navicat MySQL Data Transfer
 
-SET FOREIGN_KEY_CHECKS = 0;
+Source Server         : local
+Source Server Version : 80015
+Source Host           : localhost:3306
+Source Database       : test
 
-DROP TABLE
-IF EXISTS `users`;
+Target Server Type    : MYSQL
+Target Server Version : 80015
+File Encoding         : 65001
 
+Date: 2019-11-19 10:27:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-	`id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-	`userName` VARCHAR (32) DEFAULT NULL COMMENT '用户名',
-	`password` VARCHAR (32) DEFAULT NULL COMMENT '密码',
-	`user_sex` VARCHAR (32) DEFAULT NULL,
-	`nick_name` VARCHAR (32) DEFAULT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE = INNODB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `users`
-VALUES 
-	("aa", 11),
-	("bb", 22),
-	("cc", 33);
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'aa', '11');
+INSERT INTO `users` VALUES ('2', 'bb', '22');
+INSERT INTO `users` VALUES ('3', 'cc', '33');
